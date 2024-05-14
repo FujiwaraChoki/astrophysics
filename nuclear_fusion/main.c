8877dfd6a8446f1f41937c7ff6e81ca576ea7437
Fujiwara Choki <sami@samihindi.com>
2024-05-14 10:00:01 +0200

    chore: Update .gitignore to ignore .vscode folder

--- nuclear_fusion/main.c
+++ nuclear_fusion/main.c
@@ -1,18 +1,33 @@
+#include <stdio.h>
 #include <math.h>
 
 #define SPEED_OF_LIGHT 299792458
+#define MASS_OF_DEUTERIUM_KG 3.34358348e-27
+#define MASS_OF_TRITIUM_KG 5.007356665e-27
+#define MASS_OF_HELIUM_KG 6.6464764e-27
+#define ATOMIC_MASS_UNIT 1.6605402e-27
+#define ELECTRON_MASS -1.602e-19
 
-int main(int argc, char *argv[])
+double e_calc(double mass_deuterium, double mass_tritium, double mass_helium)
 {
-    /*
+    double mass_initial = mass_deuterium + mass_tritium;
+    double mass_final = mass_helium;
+    double mass_difference = mass_initial - mass_final;
+    double energy_release = mass_difference * SPEED_OF_LIGHT * SPEED_OF_LIGHT;
+    return energy_release;
+}
 
-    */
-    const float m_tritium = 5.0078e-27;
-    const float m_deuterium = 3.34e-27;
+float conv_ev(double num)
+{
+    return num / ELECTRON_MASS;
+}
 
-    float m_helium_4 = m_deuterium * m_tritium;
-
-    float E = (m_helium_4) * (pow(SPEED_OF_LIGHT, 2));
-
-    printf("Fusion of Deuterium and Tritium will release %f eV of Energy");
+int main()
+{
+    double energy_release;
+    energy_release = e_calc(MASS_OF_DEUTERIUM_KG / 1.6605402e-27, MASS_OF_TRITIUM_KG / 1.6605402e-27, MASS_OF_HELIUM_KG / 1.6605402e-27);
+    printf("E is equal to: %e Joules\n", energy_release);
+    float in_ev = conv_ev(energy_release);
+    printf("Released Energy in electronvolts (ev): %e", in_ev);
+    return 0;
 }
